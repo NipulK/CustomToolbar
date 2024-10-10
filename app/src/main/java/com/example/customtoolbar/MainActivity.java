afd26e6bf41960f177630fe6f74962395af84ebc
NipulK <168514285+NipulK@users.noreply.github.com>
2024-10-10 15:48:59 +0530

    second

--- app/src/main/java/com/example/customtoolbar/MainActivity.java
+++ app/src/main/java/com/example/customtoolbar/MainActivity.java
@@ -1,17 +1,14 @@
 package com.example.customtoolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,22 +17,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Setup the Toolbar
         Toolbar toolbar = findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.toolbar_menu,menu);
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
-        if(id == R.id.search){
-            Toast.makeText(this, "Searched Clicked", Toast.LENGTH_SHORT).show();
+
+        switch (id) {
+            case R.id.grouped_msg_menu:
+                Toast.makeText(this, "Grouped Messages Clicked", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.notification_menu:
+                Toast.makeText(this, "Notifications Clicked", Toast.LENGTH_SHORT).show();
+                item.setIcon(R.drawable.ic_notification_active); // Change icon dynamically
+                return true;
+
+            case R.id.profile_menu:
+                // Launch ProfileActivity when the profile menu is clicked
+                Intent intent = new Intent(this, ProfileActivity.class);
+                startActivity(intent);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
         }
-        return true;
     }
 }
